7ba4a73e1963e923b9576ac828f28468b7b9a9f6
Suguto <101311211+suguto@users.noreply.github.com>
2022-07-29 18:01:26 +0900

    first commit

---
+++ hello/src/hello/helloWorld.java
@@ -0,0 +1,19 @@
+package hello;
+
+import java.util.Scanner;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+
+	        Scanner sc = new Scanner(System.in);
+	        String line = sc.nextLine();
+	        System.out.println(line);
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ learn java/src/learn/learn.java
@@ -0,0 +1,10 @@
+package learn;
+
+public class learn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

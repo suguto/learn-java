a8910e8597a47c9d699788287fed94bdb43ecfb1
Suguto <101311211+suguto@users.noreply.github.com>
2022-08-01 14:55:07 +0900

    ループ処理やメソッドについて学びました

---
+++ hello/src/hello/roop.java
@@ -0,0 +1,25 @@
+package hello;
+
+public class roop {
+
+	public static void main(String[] args) {
+		System.out.println("”“–‚ÄƒQ[ƒ€I");
+		int ans = new java.util.Random().nextInt(10);
+		for(int i = 0; i < 3; i++) {
+			System.out.println("‚O`‚X‚Ì”š‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢");
+			int num = new java.util.Scanner(System.in).nextInt();
+			if(ans == num) {
+				System.out.println("“–‚½‚èI");
+				break;
+			}else {
+				System.out.println("‚Í`‚¸‚êI");
+			}
+		}
+		System.out.println("Šy‚µ‚©‚Á‚½H");
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}

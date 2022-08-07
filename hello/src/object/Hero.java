030f601f4b941e27ee6a48b95ec123bc7d50dac0
Suguto <101311211+suguto@users.noreply.github.com>
2022-08-07 16:20:29 +0900

    changed argument

--- hello/src/object/Hero.java
+++ hello/src/object/Hero.java
@@ -4,7 +4,7 @@ public class Hero {
 	String name;
 	int hp = 100;
 	final int MAX_HP = 100;
-	int mp = 2;
+	int mp = 20;
 	final int MAX_MP = 20;
 	public int attack(String name){
 		int damage = new java.util.Random().nextInt(3);

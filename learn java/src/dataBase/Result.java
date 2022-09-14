e9ef48d3517dd107ec7d784c299967221ca7eede
Suguto <101311211+suguto@users.noreply.github.com>
2022-09-14 11:44:05 +0900

    learn ResultSet

---
+++ learn java/src/dataBase/Result.java
@@ -0,0 +1,31 @@
+package dataBase;
+
+public class Result {
+	
+//	ResultSetが使えるメソッド
+//	メソッド名		引数		戻り値		説明
+//	next()		なし		boolean		注目する行を1つ進める、成功したらtrue、最後の行ならfalseを返す
+//	
+//	getInt()	int		int			現在注目している行について指定した列の整数値を取り出す
+//				String
+//				
+//	getString	int		String		現在注目している行について指定した列の文字列を取り出す
+//				String
+	//イテレータiteratorによく似ている
+
+	public static void main(String[] args) {
+//		//結果処理パターン（複数行）
+//		System.out.println(rs.getString("NAME"));
+//		
+//		//結果処理パターン(単一行)
+//		if (rs.next()) {
+//			System.out.println("ゴブリンのHPは" + rs.getInt("HP"));
+//		}else {
+//			System.out.println("ゴブリンはありませんでした");
+//		}
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- learn java/src/dataBase/SqlSentence.java
+++ learn java/src/dataBase/SqlSentence.java
@@ -2,8 +2,11 @@ package dataBase;
 import java.sql.*;
 
 public class SqlSentence {
+	
+	//更新系SQL文と検索系SQL文送信の違いはexecuteUpdate(), executeQuery()	
 
 	public static void main(String[] args) {
+		//更新系のSQL文
 		//STEP1 送信すべきSQL文の雛形を準備
 //		PreparedStatement pstmt = con.prepareStatement("DELETE FROM MONSTER HERER HP <= ? OR NAME = ?");
 //		
@@ -21,7 +24,24 @@ public class SqlSentence {
 //			System.out.println("該当するモンスターはありませんでした");
 //		}
 //		pstmt.close();
-	
+		
+		//検索系のSQL文
+//		STEP1　送信すべきSQL文の雛形を準備
+//		PrepareStatement pstmt = con.prepareStatement("SELECT MONSTERS WHEHE HP >= ?");
+//		
+//		STEP2　ひな形に値を流し込みSQL文を組み立てる
+//		pstmt.setInt(1,10);
+//		
+//		STEP3 組み立て終えたSQL文をDBMSに送信する
+//		ResultSet rs = pstmt.executeQuery();
+//		
+//		STEP4 結果票を処理する
+//		Resultクラスを参照
+//		
+//		rs.close();
+//		pstmt.close();
+		
+		
 		
 		// TODO Auto-generated method stub
 
